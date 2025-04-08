6b9d0a69915de471f90dd4ee92a67cb29522c975
David Jiang <jiang.david23@gmail.com>
2025-04-08 13:59:09 -0400

    added login and register functionality and errors

--- app.py
+++ app.py
@@ -1,13 +1,46 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, url_for, request, flash, redirect
+
+userDatabase = {"username123": "password123"}
 
 app = Flask(__name__)
+app.secret_key = "super secret key"
 
-@app.route('/')
+@app.route('/', methods=['GET','POST'])
 def register():
-    return render_template('register.html')
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+        confirmPassword = request.form['confirmPassword']
+        
+        if username in userDatabase.keys():
+            flash("Username Already Exists", category="error")
+            return redirect('/')
+        
+        if password != confirmPassword:
+            flash("Passwords Must Match", category="error")
+            return redirect('/')
+        
+        userDatabase[username] = password
+        return "DONE"
+    else:
+        return render_template('register.html')
 
-@app.route('/login.html')
+@app.route('/login.html', methods=['GET', 'POST'])
 def login():
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+        
+        if username not in userDatabase.keys():
+            flash("Username not Found")
+            return redirect('/login.html')
+        
+        if userDatabase[username] != password:
+            flash("Password is Incorrect")
+            return redirect('/login.html')
+        
+        return "DONE"
+        
     return render_template('login.html')
 
 if __name__ == "__main__":
