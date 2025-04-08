bf1a68e650be19b53d833b931bb4cf4b34ea5cb5
David Jiang <jiang.david23@gmail.com>
2025-04-07 20:28:38 -0400

    added buttons to switch between register and login page

--- app.py
+++ app.py
@@ -3,8 +3,12 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route('/')
-def index():
+def register():
     return render_template('register.html')
+
+@app.route('/login.html')
+def login():
+    return render_template('login.html')
 
 if __name__ == "__main__":
     app.run(debug=True)
